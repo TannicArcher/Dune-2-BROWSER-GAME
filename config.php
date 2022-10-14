d626963fd04a3ab35cd42030713056f4d863f499
Руслан <113786861+TannicArcher@users.noreply.github.com>
2022-10-14 12:43:00 +0300

    add files

---
+++ config.php
@@ -0,0 +1,22 @@
+<?
+extract($_GET);
+extract($_POST);
+// MySQL Settings: Host, Base, User, Password
+
+$sys_dbhost="localhost";
+$sys_dbname="dune";
+$sys_dbuser="dune";
+$sys_dbpass="dunepass";
+
+$cfg_cosmo_vrag=1;
+$dune_x=150;
+$dune_y=150;
+$disbalance=10;
+$granica=true;
+$raund=22;
+$deltime=2000;
+$max_f=15;
+$sleep_d=1;
+$price_=1;
+
+?>
